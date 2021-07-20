504c8138dac565502d275330598e635829120b36
Kuggo <77054567+Kuggo@users.noreply.github.com>
2021-07-20 10:21:35 +0100

    Add files via upload

---
+++ libraries/advmath.py
@@ -0,0 +1,56 @@
+def sqrt(a, b):
+    return f'''{}'''
+
+
+def smlt(a, b , c):
+    return f'''{}'''
+
+
+def sdiv(a, b, c):
+    return f'''{}'''
+
+
+def sqr(a, b):
+    return f'''{}'''
+
+
+def exp(a, b):
+    return f'''{}'''
+
+
+def log(a, b):
+    return f'''{}'''
+
+
+def ln(a, b):
+    return f'''{}'''
+
+
+def rdwn(a, b, c):
+    return f'''{}'''
+
+
+def rup(a, b, c):
+    return f'''{}'''
+
+
+def gcf(a, b):
+    return f'''{}'''
+
+
+def lcm(a, b, c):
+    return f'''{}'''
+
+
+def abs(a, b):
+    return f'''{}'''
+
+
+def min(a, b, c):
+    return f'''{}'''
+
+
+def max(a, b, c):
+    return f'''{}'''
+
+
---
+++ libraries/float.py
@@ -0,0 +1,32 @@
+def add(a, b, c):
+    return f''''''
+
+
+def sub(a, b, c):
+    return f''''''
+
+
+def mlt(a, b, c):
+    return f''''''
+
+
+def div(a, b, c):
+    return f''''''
+
+
+def bshift(a, b, c):
+    return f''''''
+
+
+def sqrt(a, b):
+    return f''''''
+
+
+def square(a, b):
+    return f''''''
+
+
+def move(a, b):
+    return f''''''
+
+
